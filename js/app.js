06a3856b4eae418578386866f05f1bf3f5ef5727
Clau1889 <clau.blanco1889@gmail.com>
2018-05-15 22:56:57 -0500

    En este commit se sube la version 2 del ejercicio, quedando pendiente acomodar hacia la izquierda cada lista en lugar de a la derecha, pero la funcionalidad si se realiza como se esperaba

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
-var formulario= function (event){
-    //Crear Etiquetas que necesito
+var formulario= function (e){
+    e.preventDefault();
+    //Crear Etiquetas para el formulario
     var tagA= document.getElementById("add-list");
         tagA.style.display= "none";  
     var sectionForm= document.getElementById("section-form");
@@ -7,11 +8,11 @@ var formulario= function (event){
         tagForm.setAttribute("id", "form-box");
     var tagInput= document.createElement("input");
         tagInput.setAttribute("id", "form-input");
-        tagInput.setAttribute("placeholder", "Add List");
+        tagInput.setAttribute("placeholder", "Add a list...");
     var tagDiv= document.createElement("div");
     var tagButton= document.createElement("button");
         tagButton.textContent = "Save";
-        tagButton.setAttribute("class", "form-button");
+        tagButton.setAttribute("id", "save-button");
     var tagSecondButton= document.createElement("button");
         tagSecondButton.innerHTML = "&times;";
         tagSecondButton.setAttribute("id", "close-button");
@@ -23,16 +24,42 @@ var formulario= function (event){
     tagForm.appendChild(tagDiv);
     sectionForm.appendChild(tagForm);
     sectionForm.style.display= "block";
+
+
     //Agregar evento al close-button
     var close= document.getElementById("close-button");
     close.addEventListener("click",limpiar);
 
     //Agregar evento para guardar contenido de Input
-    //var close= document.getElementById("close-button");
-    //close.addEventListener("click",limpiar);
+var save= document.getElementById("save-button");
+    save.addEventListener('click', function(e){
+    e.preventDefault();
+    //crear todas las etiquetas para la section de list
+    var saveButton= document.getElementById("form-input").value;
+    var textInput= document.createTextNode(saveButton);
+    var listBox= document.getElementById("section-new-list");
+    var divList= document.createElement('div');
+        divList.setAttribute("id","box-list");
+    var title= document.createElement("h3");
+        title.setAttribute("id","title-list");
+    var inputList= document.createElement("a");
+        inputList.textContent= "Add a card...";
+        inputList.setAttribute("id","title-links");
+
+        //desginar lugar en la familia
+        listBox.appendChild(divList);
+        divList.appendChild(title);
+        title.appendChild(textInput);
+        divList.appendChild(inputList);
+
+        //Limpiar el input 
+        document.getElementById("form-input").value= ""; 
+
+    });
 };
 
-var limpiar= function(event){
+//Oculta el cuadro de link para mostrar el de agregar la lista
+var limpiar= function(){
     var hidden= document.getElementById("section-form");
     hidden.style.display= "none";    
     hidden.innerHTML = ""; 
@@ -40,8 +67,7 @@ var limpiar= function(event){
     show.style.display= "block"; 
 };
 
-//Agregar funcion para generar la lista
-//crear todas las etiquetas de big-box y asignarle el evento
+
 
 //Primera version: agregar evento al boton.
 var addList= document.getElementById("add-list");
